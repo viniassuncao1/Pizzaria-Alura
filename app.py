dbe4bdcf4edf2072e890d9e3705e40fd96c2edfb
viniassuncao1 <viniciusmassuncao@gmail.com>
2025-03-26 14:58:38 -0300

    Criando classe abstrata e adicionando nas subclasses

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
